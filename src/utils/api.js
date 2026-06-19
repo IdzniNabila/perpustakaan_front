6a16295b5300371e97bc0d0db374c2683f2e55ff
IdzniNabila <idzni1504@gmail.com>
2026-06-19 11:00:35 +0700

    vercel

--- src/utils/api.js
+++ src/utils/api.js
@@ -12,7 +12,7 @@ try {
 }
 
 const api = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api',
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json',
